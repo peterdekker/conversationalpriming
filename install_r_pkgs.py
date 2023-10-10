ee572b927159294f0321f1501c78dc571dc5cdb9
Peter Dekker <pdekker@ai.vub.ac.be>
2023-10-10 14:13:15 +0200

    Moved data analysis of person markers to own repository

--- install_r_pkgs.py
+++
@@ -1,6 +0,0 @@
-packnames = ('stringi','lme4','tidyverse', 'ggeffects', 'afex') #, 'lmerTest', 'emmeans', "geepack")
-from rpy2.robjects.packages import importr
-from rpy2.robjects.vectors import StrVector
-utils = importr("utils")
-utils.chooseCRANmirror(ind=1)
-utils.install_packages(StrVector(packnames))
